dea555fd45660eb11e95ba2477658bca28409479
Reki-code <weiruiqi@hotmail.com>
2021-02-18 18:07:15 +0800

    simple apollo server

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const cors = require('cors')
+const { ApolloServer, gql } = require('apollo-server-express')
+ 
+const app = express()
+app.use(cors())
+ 
+const schema = gql`
+  type Query {
+    me: User
+  }
+  type User {
+    username: String!
+  }
+`
+const resolvers = {
+  Query: {
+    me: () => {
+      return {
+        username: 'Rekii W',
+      }
+    }
+  }
+}
+ 
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+ 
+server.applyMiddleware({ app, path: '/graphql' });
+ 
+app.listen({ port: 4000 }, () => {
+  console.log('Apollo Server on http://localhost:4000/graphql');
+})
